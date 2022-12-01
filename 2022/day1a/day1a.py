d28d1c154dad8732244964234d9769473b8254b9
cortomaltese <giorgos.kalomalos@outlook.com>
2022-12-01 12:28:59 +0200

    refactor/Change code to increase performance and readability
    - Remove one for loop to increase performance.
    - Add comments to increase readability.

--- 2022/day1a/day1a.py
+++ 2022/day1a/day1a.py
@@ -1,21 +1,22 @@
 import numpy as np
 
 inventory = []
+# Read inventory.txt file into data object
 with open("inventory.txt", "r") as file:
     data = file.read().split("\n")
-    temp_inventory = []
-    for item in data:
-        if item:
-            temp_inventory.append(int(item))
-        else:
-            inventory.append(temp_inventory)
-            temp_inventory = []
 
-
+temp_inventory = []
 max_calories = 0
-for item in inventory:
-    item_sum = np.sum(item)
-    if item_sum > max_calories:
-        max_calories = item_sum
+# While there is no empty line add data to temp_inventory list.
+# When empty row is found, the sum of the items before is calculated.
+# If the sum is the largest, the max_calories is updated
+for item in data:
+    if item:
+        temp_inventory.append(int(item))
+    else:
+        item_sum_calories = np.sum(temp_inventory)
+        temp_inventory = []
+        if item_sum_calories > max_calories:
+            max_calories = item_sum_calories
 
-print(max_calories)
+print(max_calories)  # 66487
